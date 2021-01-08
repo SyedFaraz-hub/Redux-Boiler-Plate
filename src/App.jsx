3f138c3fb8bb44c2caf5df9c5ff611d266c44089
SyedFaraz-hub <74416299+SyedFaraz-hub@users.noreply.github.com>
2021-01-08 20:26:05 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './App.css'
+import { connect } from "react-redux";
+import { upload_data } from "../src/store/action";
+
+class App extends React.Component {
+
+  render() {
+    let newUser = {
+
+      name: "Ali",
+      email: "Ali@gmail.com",
+
+    }
+          console.log(this.props.users)
+    return (
+      <div>
+        <h1>Working Fine...</h1>
+
+        <button onClick={() => { this.props.SET_DATA(newUser) }} >SET DATA</button>
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = (state) => ({
+
+  users: state.users
+})
+
+const mapDispatchToProps = (dispatch) => ({
+
+  SET_DATA: (newUser) => {dispatch(upload_data(newUser))}
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
